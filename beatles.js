f02ed45212b2d000d3bbb1f4e29fbc234fe11cd9
nanselmo <nickianselmo@gmail.com>
2016-02-27 15:13:31 -0600

    changed iLovetheBeatles() function

--- beatles.js
+++ beatles.js
@@ -24,10 +24,11 @@ function johnLennonFacts(){
 }
 
 function iLoveTheBeatles(num){
+  var i = 0
   var beatleMania = []
   do {
     beatleMania.push("I love the Beatles!")
-    num ++;
-  } while (num <15);
+    i++;
+  } while (i <num && i<10);
   return beatleMania;
 }
